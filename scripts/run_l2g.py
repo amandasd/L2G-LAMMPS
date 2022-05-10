ce4308cc710e93043b8a1a18118431d22049cee8
amanda <amanda.dufek@gmail.com>
2022-05-10 14:53:21 -0700

    Update

--- scripts/run_l2g.py
+++ scripts/run_l2g.py
@@ -71,7 +71,7 @@ parser.add_argument("-ts", "--tournament-size", type=int, default=3, help="tourn
 parser.add_argument("-best", "--number-of-retained-solutions", type=int, default=4, help="number of best candidates selected to generate new candidates [default=4]")
 parser.add_argument("-e", "--elitism", type=bool, default=True, help="elitism [default=True]")
 
-# neural network parameters 
+# neural network parameters
 parser.add_argument("-hid", "--number-of-hidden-nodes", type=int, default=10, help="number of hidden nodes [default=10]")
 
 # temperature and pressure parameters
@@ -93,14 +93,14 @@ n_pop     = args.population_size
 mut_rate  = args.mutation_rate
 mut_sigma = args.mutation_sigma
 ts        = args.tournament_size
-n_best    = args.number_of_retained_solutions 
+n_best    = args.number_of_retained_solutions
 
 if mut_rate > 1 or mut_rate < 0:
     mut_rate = 1
 if n_best > n_pop:
     n_best = int(np.ceil(n_pop * 0.1))
 
-# define neural network parameters 
+# define neural network parameters
 input_nodes  = 1
 hidden_nodes = args.number_of_hidden_nodes
 output_nodes = 2
@@ -185,29 +185,22 @@ def run_networks(pop, temp, press, node_input, n, gen):
         weight_ih = np.reshape(weight_ih,(input_nodes,hidden_nodes))
         weight_ho = pop[p][hidden_nodes+input_nodes*hidden_nodes:hidden_nodes+input_nodes*hidden_nodes+output_nodes*hidden_nodes]
         weight_ho = np.reshape(weight_ho,(hidden_nodes,output_nodes))
- 
-        # calculate Sj 
+
+        # calculate Sj
         node_hidden = np.zeros(shape=(hidden_nodes), dtype=float)
         for j in range(hidden_nodes):
             node_hidden[j] = np.sum(np.dot(node_input,weight_ih[:,j]))
         node_hidden = np.tanh(np.add(node_hidden,bias_hidden))
-        
-        # calculate Sk 
+
+        # calculate Sk
         node_output = np.zeros(shape=(output_nodes), dtype=float)
         for k in range(output_nodes):
             node_output[k] = np.sum(np.dot(node_hidden,weight_ho[:,k]))/(1.*hidden_nodes)
 
-        temp[p] += node_output[0] * args.temperature_factor
-        if temp[p] > bounds[0][1]:
-            temp[p] = bounds[0][1]
-        if temp[p] < bounds[0][0]:
-            temp[p] = bounds[0][0]
-        
-        press[p] += node_output[1] * args.pressure_factor
-        if press[p] > bounds[1][1]:
-            press[p] = bounds[1][1]
-        if press[p] < bounds[1][0]:
-            press[p] = bounds[1][0]
+        press[p] += node_output[1] * args.pressure_factor * random.choice([1,-1])
+        temp[p]  += node_output[0] * args.temperature_factor * random.choice([1,-1])
+        if temp[p] < 0:
+            temp[p] = 0
 
         with open("output/protocol-"+str(p),"a") as outfile:
             outfile.write("gen {}, step {}, {}, {}\n".format(gen,int(node_input*lmp.n_steps),temp[p],press[p]))
@@ -221,32 +214,32 @@ def evaluate(pop, gen, n):
     # initialize temperature and pressure values: 0 (random), 1 (fixed values), 2 (mutated from a given value)
     # options 1 and 2 require initial temperature and pressure values
     # arguments: population size, option (0, 1, 2), initial temperature value, initial pressure value
-    temp, press = initialize_T_P(n, args.initialize_T_P, args.initial_temperature, args.initial_pressure) 
+    temp, press = initialize_T_P(n, args.initialize_T_P, args.initial_temperature, args.initial_pressure)
 
-    # run LAMMPS with initial structure 
+    # run LAMMPS with initial structure
     if gen <= n_gen:
         print("[gen %s] running LAMMPS with initial structure" %str(gen))
-        lmp.run_lammps(temp, press, 0, gen, n_pop, args.number_of_gpus) 
+        lmp.run_lammps(temp, press, 0, gen, n_pop, args.number_of_gpus)
     else:
         print("running LAMMPS with initial structure for best solution")
         lmp.best_lammps(temp, press, 0, gen)
 
     for s in range(lmp.n_steps):
-        node_input = s * 1./lmp.n_steps 
+        node_input = s * 1./lmp.n_steps
         # run neural networks
         temp, press = run_networks(pop, temp, press, node_input, n, gen)
         state = s*lmp.npt_steps+lmp.npt_steps+lmp.nve_steps
         # run LAMMPS with restart files
         if gen <= n_gen:
             print("[gen %s; step %s] running LAMMPS with restart file" %(str(gen), str(s)))
-            lmp.run_lammps(temp, press, state, gen, n_pop, args.number_of_gpus) 
+            lmp.run_lammps(temp, press, state, gen, n_pop, args.number_of_gpus)
         else:
             print("[step %s] running LAMMPS with restart file for best solution" %(str(s)))
             lmp.best_lammps(temp, press, state, gen)
 
     # calculate scores
     scores = lmp.get_scores(gen, n, n_gen)
-    
+
     lmp.delete_output_files(gen, n, n_gen)
 
     return scores
@@ -270,13 +263,13 @@ if __name__ == '__main__':
     os.system('rm -f output/protocol*')
 
     random.seed(datetime.now())
-    
+
     # restart L2G from the last state in case it was interrupted
     if args.restart:
         value = -1000
         lines = open("output/restart.dat","r").readlines()
         for line_idx, data in enumerate(lines):
-            gen_scores = eval(data.split("|")[1])    
+            gen_scores = eval(data.split("|")[1])
             if max(gen_scores) > value:
                 pop = eval(data.split("|")[0])
                 scores = eval(data.split("|")[1])
@@ -287,13 +280,13 @@ if __name__ == '__main__':
         # evaluate all candidates in the population: run neural networks and LAMMPS
         scores = evaluate(pop, 0, n_pop)
 
-    # select the best candidate 
+    # select the best candidate
     idx = scores.index(max(scores))
     best_ind, best_score = pop[idx], scores[idx]
     print()
     print(">0, new best = %f" % (best_score))
     print()
-    
+
     # save generation, best index, and best score in an output file
     with open("output/dumpfile.dat","a") as outfile1:
         outfile1.write("{} {} {}\n".format(0, idx, best_score))
@@ -301,14 +294,15 @@ if __name__ == '__main__':
     for gen in range(n_gen): # maximum number of iterations
 
         elitism = args.elitism
-        
-        # rank the scores 
+
+        # rank the scores
         indices = [scores.index(x) for x in sorted(scores, reverse=True)]
-    
+
         # select parents from the current population
         # n_best candidates are selected to generate new candidates
-        selected = [selection(np.take(pop,indices,0)[:n_best], np.take(scores,indices,0)[:n_best]) for _ in range(n_pop)]
-        
+        selected_idx = list(np.array(indices)[np.array(randint(0, n_best, n_pop))])
+        selected = list(np.array(pop)[np.array(selected_idx)])
+
         # create the next generation
         new_pop = list()
         for i in range(0, n_pop):
@@ -324,14 +318,14 @@ if __name__ == '__main__':
             new_pop.append(ind)
         # replace population
         pop = new_pop
-    
+
         # evaluate all candidates in the population: run neural networks and LAMMPS
         scores = evaluate(pop, gen+1, n_pop)
-    
+
         # save population and scores in order to restart L2G from the last state in case of being interrupted
         with open("output/restart.dat","a") as outfile2:
             outfile2.write("{} | {}\n".format(pop,scores))
-    
+
         # select the best candidate
         idx = scores.index(max(scores))
         if scores[idx] > best_score:
@@ -339,11 +333,11 @@ if __name__ == '__main__':
             print()
             print(">%d, new best = %f" % (gen+1, best_score))
             print()
-    
+
         # save generation, best index, and best score in an output file
         with open("output/dumpfile.dat","a") as outfile1:
             outfile1.write("{} {} {}\n".format(gen+1, idx, best_score))
-    
+
     # evaluate the best candidate
     #scores = evaluate([pop[idx]], n_gen+1, 1)
     #print("best = %f" % (scores[0]))
--- scripts/submit.sh
+++ scripts/submit.sh
@@ -8,15 +8,15 @@
 #SBATCH -n 40
 #SBATCH -c 2
 #SBATCH --switches=1
-#SBATCH --exclusive     
+#SBATCH --exclusive
 #SBATCH -J learningToGrow
 #SBATCH --output=l2g.out
 
 HOME="$(pwd)"
 
-module purge 
+module purge
 module load cgpu
-module load cmake 
+module load cmake
 module load PrgEnv-llvm/12.0.0-git_20210117
 module load python/3.8-anaconda-2020.11
 
@@ -24,7 +24,6 @@ export LAMMPS_DIR=$HOME
 export OMP_NUM_THREADS=1
 
 date
-#python3.8 $HOME/scripts/run_l2g.py -gpus 8
-python3.8 $HOME/scripts/run_l2g.py -gpus 8 -pop 16 -gen 8 -tmin 200 -tmax 400 -pmin 100000 -pmax 400000 -opt 0 -tf 10 -pf 15000 -ms 0.5
-#python3.8 $HOME/scripts/run_l2g.py -gpus 8 -pop 16 -gen 30 -tmin 200 -tmax 400 -pmin 100000 -pmax 400000 -opt 1 -vtemp 375 -vpress 150000 -tf 10 -pf 15000 -ms 0.5
+#T and P -> high score
+python3.8 $HOME/scripts/run_l2g.py -gpus 8 -pop 16 -gen 8 -tmin 200 -tmax 400 -pmin 100000 -pmax 400000 -opt 1 -vtemp 200 -vpress 355467 -tf 10 -pf 15000 -ms 0.01 -hid 1000
 date
